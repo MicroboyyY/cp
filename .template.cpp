8da3ee889402b0da6fb94ff2d7b8962f758b7b74
odd.c <tanimahmed710@gmail.com>
2024-04-07 17:36:03 +0600

    adsbfuad

--- .template.cpp
+++ .template.cpp
@@ -1,20 +1,13 @@
 #include <bits/stdc++.h>
 using namespace std;
-#ifdef LOCAL
-// #define cerr cout
-#include "algo/debug.h"
-#else
-#define deb(...)  30
-#endif
+
 #define ll long long
-#define all(x) (x).begin(),(x).end()
+#define all(x) (x).begin(), (x).end()
 #define f(i, n) for (int i = 0; i < n; i++)
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
-    
-    return 0;
+
 }
--- cf.cpp
+++ cf.cpp
@@ -1,22 +1,12 @@
 #include <bits/stdc++.h>
 using namespace std;
-#ifdef LOCAL
-#include "algo/debug.h"
-// #define cerr cout
-#else
-#define deb(...) 122
-#endif
+
 #define ll long long
-const int N = 1e5 + 10;
 #define all(x) (x).begin(), (x).end()
 #define f(i, n) for (int i = 0; i < n; i++)
 
-int main()
-{
+int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    cout << int(log2(7));
-
-    return 0;
+    
 }
