7bc7d9dc857071a7ed796cf73f2c4e7049b59d80
odd.c <tanimahmed710@gmail.com>
2024-04-14 15:21:50 +0600

    testing

--- cf.cpp
+++ cf.cpp
@@ -4,7 +4,7 @@ using namespace std;
 #define ll long long
 #define all(x) (x).begin(), (x).end()
 #define f(i, n) for (int i = 0; i < n; i++)
-
+#define trace(x) cerr << #x << ": " << x << '\n'
 int main() {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
