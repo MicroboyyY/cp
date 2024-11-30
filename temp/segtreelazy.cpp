8e5412236aa6d553726df811c8e1ddb62e1007dc
odd.c <tanimahmed710@gmail.com>
2024-11-30 17:52:20 +0600

    as

--- temp/segtreelazy.cpp
+++ temp/segtreelazy.cpp
@@ -1,81 +1,82 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-#define ll long long
-#define all(x) (x).begin(), (x).end()
-#define f(i, n) for (int i = 0; i < n; i++)
-#define trace(x) cerr << #x << ": " << x << '\n'
-const int N = 100007;
-#define int long long
-int arr[N];
-ll t[4 * N], lazy[4 * N];
-ll marge(int l, int r) {
-    return t[l] + t[r];
-}
-void push(int n, int b, int e) {
-    if (lazy[n] == -1)
-        return;
-    t[n] = (e - b + 1) * lazy[n];
-    if (b != e) {
-        int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-        lazy[l] = lazy[n];
-        lazy[r] = lazy[n];
+const int N = 5e5 + 9;
+int a[N];
+struct ST
+{
+#define lc (n << 1)
+#define rc ((n << 1) | 1)
+    long long t[4 * N], lazy[4 * N];
+    ST()
+    {
+        memset(t, 0, sizeof t);
+        memset(lazy, 0, sizeof lazy);
     }
-    lazy[n] = -1;
-}
-void build(int n, int b, int e) {
-    lazy[n] = -1;
-    if (b == e) {
-        t[n] = arr[b];
-        return;
+    inline void push(int n, int b, int e)
+    {
+        if (lazy[n] == 0)
+            return;
+        t[n] = t[n] + lazy[n] * (e - b + 1);
+        if (b != e)
+        {
+            lazy[lc] = lazy[lc] + lazy[n];
+            lazy[rc] = lazy[rc] + lazy[n];
+        }
+        lazy[n] = 0;
     }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    build(l, b, mid), build(r, mid + 1, e);
-    t[n] = marge(l, r);
-}
-void upd(int n, int b, int e, int i, int j, int v) {
-    push(n, b, e);
-    if (e < i or b > j)
-        return;
-    if (i <= b and j >= e) {
-        lazy[n] = v;
+    inline long long combine(long long a, long long b)
+    {
+        return a + b;
+    }
+    inline void pull(int n)
+    {
+        t[n] = t[lc] + t[rc];
+    }
+    void build(int n, int b, int e)
+    {
+        lazy[n] = 0;
+        if (b == e)
+        {
+            t[n] = a[b];
+            return;
+        }
+        int mid = (b + e) >> 1;
+        build(lc, b, mid);
+        build(rc, mid + 1, e);
+        pull(n);
+    }
+    void upd(int n, int b, int e, int i, int j, long long v)
+    {
         push(n, b, e);
-        return;
-    }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    upd(l, b, mid, i, j, v), upd(r, mid + 1, e, i, j, v);
-    t[n] = marge(l, r);
-}
-ll query(int n, int b, int e, int i, int j) {
-    push(n, b, e);
-    if (e < i or b > j)
-        return 0;
-    if (i <= b and j >= e) {
-        return t[n];
-    }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    return (query(l, b, mid, i, j) + query(r, mid + 1, e, i, j));
-}
-int32_t main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, q;
-    cin >> n >> q;
-    build(1, 1, n);
-    while (q--) {
-        int ty;
-        cin >> ty;
-        if (ty == 1) {
-            int l, r, v;
-            cin >> l >> r >> v;
-            l++;
-            upd(1, 1, n, l, r, v);
+        if (j < b || e < i)
+            return;
+        if (i <= b && e <= j)
+        {
+            lazy[n] = v; // set lazy
+            push(n, b, e);
+            return;
         }
-        else {
-            int i;
-            cin >> i;
-            i++;
-            cout << query(1, 1, n, i, i) << '\n';
-        }
+        int mid = (b + e) >> 1;
+        upd(lc, b, mid, i, j, v);
+        upd(rc, mid + 1, e, i, j, v);
+        pull(n);
     }
+    long long query(int n, int b, int e, int i, int j)
+    {
+        push(n, b, e);
+        if (i > e || b > j)
+            return 0; // return null
+        if (i <= b && e <= j)
+            return t[n];
+        int mid = (b + e) >> 1;
+        return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
+    }
+};
+int32_t main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    return 0;
 }
