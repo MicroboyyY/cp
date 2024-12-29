77153223fb97ffd813a6d99c0195759bfabaa788
odd.c <tanimahmed710@gmail.com>
2024-12-29 21:36:17 +0600

    ok

--- .template.cpp
+++ .template.cpp
@@ -2,10 +2,12 @@
 using namespace std;
 
 #define ll long long
+#define int long long
 #define all(x) (x).begin(), (x).end()
 #define f(i, n) for (int i = 0; i < n; i++)
 #define trace(x) cerr << #x << ": " << x << '\n'
-int main() {
+int32_t main()
+{
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     
