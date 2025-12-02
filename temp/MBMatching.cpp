33ccd09912fd39d834b2a44bc102087c2c661764
odd.c <tanimahmed710@gmail.com>
2025-12-02 15:36:40 +0600

    ok

--- temp/MBMatching.cpp
+++ temp/MBMatching.cpp
@@ -1,36 +1,47 @@
-#include<bits/stdc++.h>
+#include <bits/stdc++.h>
 using namespace std;
 
 const int N = 3e5 + 9;
 
-struct HopcroftKarp {
+struct HopcroftKarp
+{
   static const int inf = 1e9;
   int n;
-  vector<int> l, r, d;
+  vector<int> l, r, d, vis;
   vector<vector<int>> g;
-  HopcroftKarp(int _n, int _m) {
+  HopcroftKarp(int _n, int _m)
+  {
     n = _n;
     int p = _n + _m + 1;
     g.resize(p);
     l.resize(p, 0);
     r.resize(p, 0);
     d.resize(p, 0);
+    vis.resize(p, 0);
   }
-  void add_edge(int u, int v) {
-    g[u].push_back(v + n); //right id is increased by n, so is l[u]
+  void add_edge(int u, int v)
+  {
+    g[u].push_back(v + n); // right id is increased by n, so is l[u]
   }
-  bool bfs() {
+  bool bfs()
+  {
     queue<int> q;
-    for (int u = 1; u <= n; u++) {
-      if (!l[u]) d[u] = 0, q.push(u);
-      else d[u] = inf;
+    for (int u = 1; u <= n; u++)
+    {
+      if (!l[u])
+        d[u] = 0, q.push(u);
+      else
+        d[u] = inf;
     }
     d[0] = inf;
-    while (!q.empty()) {
+    while (!q.empty())
+    {
       int u = q.front();
       q.pop();
-      for (auto v : g[u]) {
-        if (d[r[v]] == inf) {
+      for (auto v : g[u])
+      {
+        if (d[r[v]] == inf)
+        {
           d[r[v]] = d[u] + 1;
           q.push(r[v]);
         }
@@ -38,10 +49,14 @@ struct HopcroftKarp {
     }
     return d[0] != inf;
   }
-  bool dfs(int u) {
-    if (!u) return true;
-    for (auto v : g[u]) {
-      if(d[r[v]] == d[u] + 1 && dfs(r[v])) {
+  bool dfs(int u)
+  {
+    if (!u)
+      return true;
+    for (auto v : g[u])
+    {
+      if (d[r[v]] == d[u] + 1 && dfs(r[v]))
+      {
         l[u] = v;
         r[v] = u;
         return true;
@@ -50,21 +65,43 @@ struct HopcroftKarp {
     d[u] = inf;
     return false;
   }
-  int maximum_matching() {
+  int maximum_matching()
+  {
     int ans = 0;
-    while (bfs()) {
-      for(int u = 1; u <= n; u++) if (!l[u] && dfs(u)) ans++;
+    while (bfs())
+    {
+      for (int u = 1; u <= n; u++)
+        if (!l[u] && dfs(u))
+          ans++;
     }
     return ans;
   }
+  bool yo(int u) // it show can we find an augmenting path from u or matching of u
+  {
+    if (vis[u])
+      return false;
+    vis[u] = true;
+    for (auto v : g[u])
+    {
+      if (r[v] == 0 || yo(r[v]))
+      {
+        l[u] = v;
+        r[v] = u;
+        return true;
+      }
+    }
+    return false;
+  }
 };
-int32_t main() {
+int32_t main()
+{
   ios_base::sync_with_stdio(0);
   cin.tie(0);
   int n, m, q;
   cin >> n >> m >> q;
   HopcroftKarp M(n, m);
-  while (q--) {
+  while (q--)
+  {
     int u, v;
     cin >> u >> v;
     M.add_edge(u, v);
