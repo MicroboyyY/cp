3c6ca654e90086036d2db9934769680f3bb9cb1e
odd.c <tanimahmed710@gmail.com>
2025-12-08 01:27:59 +0600

    asd

--- temp/PST_lazy.cpp
+++ temp/PST_lazy.cpp
@@ -3,112 +3,102 @@ using namespace std;
 
 const int N = 2e5 + 9;
 
-struct SegTree
+struct PST
 {
 #define lc t[cur].l
 #define rc t[cur].r
     struct node
     {
-        int l = 0, r = 0;
-        int val = 0;
-        int lazy = 0;
-    } t[4 * N];
+        int l = 0, r = 0, val = 0, lazy = 0;
+    } t[40 * N];
     int T = 0;
 
-    void build(int cur, int b, int e, int a[])
+    int build(int b, int e)
     {
+        int cur = ++T;
+        t[cur].l = t[cur].r = 0;
+        t[cur].val = 0;
+        t[cur].lazy = 0;
         if (b == e)
-        {
-            t[cur].val = a[b];
-            return;
-        }
+            return cur;
         int mid = (b + e) >> 1;
-        lc = cur << 1;
-        rc = cur << 1 | 1;
-        build(lc, b, mid, a);
-        build(rc, mid + 1, e, a);
-        t[cur].val = t[lc].val + t[rc].val;
+        t[cur].l = build(b, mid);
+        t[cur].r = build(mid + 1, e);
+        t[cur].val = t[t[cur].l].val + t[t[cur].r].val;
+        return cur;
     }
 
     void push(int cur, int b, int e)
     {
-        if (t[cur].lazy == 0)
+        if (t[cur].lazy == 0 || b == e)
             return;
-        t[cur].val += (e - b + 1) * t[cur].lazy;
-        if (b != e)
-        {
-            lc = cur << 1;
-            rc = cur << 1 | 1;
-            t[lc].lazy += t[cur].lazy;
-            t[rc].lazy += t[cur].lazy;
-        }
+        int mid = (b + e) >> 1;
+        int oldL = t[cur].l;
+        int oldR = t[cur].r;
+
+        int newL = ++T;
+        t[newL] = t[oldL];
+        t[newL].lazy += t[cur].lazy;
+        t[newL].val += t[cur].lazy * (mid - b + 1);
+
+        int newR = ++T;
+        t[newR] = t[oldR];
+        t[newR].lazy += t[cur].lazy;
+        t[newR].val += t[cur].lazy * (e - mid);
+
+        t[cur].l = newL;
+        t[cur].r = newR;
         t[cur].lazy = 0;
     }
 
-    void upd(int cur, int b, int e, int l, int r, int v)
+    int upd(int pre, int b, int e, int l, int r, int v)
     {
-        push(cur, b, e);
         if (b > r || e < l)
-            return;
+            return pre;
+        int cur = ++T;
+        t[cur] = t[pre];
         if (b >= l && e <= r)
         {
             t[cur].lazy += v;
-            push(cur, b, e);
-            return;
+            t[cur].val += v * (e - b + 1);
+            return cur;
         }
+        push(cur, b, e);
         int mid = (b + e) >> 1;
-        lc = cur << 1;
-        rc = cur << 1 | 1;
-        upd(lc, b, mid, l, r, v);
-        upd(rc, mid + 1, e, l, r, v);
-        t[cur].val = t[lc].val + t[rc].val;
+        int L = upd(t[cur].l, b, mid, l, r, v);
+        int R = upd(t[cur].r, mid + 1, e, l, r, v);
+        t[cur].l = L;
+        t[cur].r = R;
+        t[cur].val = t[L].val + t[R].val;
+        return cur;
     }
 
-    int query(int cur, int b, int e, int l, int r)
+    int query(int cur, int b, int e, int l, int r, int add = 0)
     {
-        push(cur, b, e);
-        if (b > r || e < l)
+        if (!cur || b > r || e < l)
             return 0;
         if (b >= l && e <= r)
-            return t[cur].val;
+            return t[cur].val + add * (e - b + 1);
         int mid = (b + e) >> 1;
-        lc = cur << 1;
-        rc = cur << 1 | 1;
-        return query(lc, b, mid, l, r) + query(rc, mid + 1, e, l, r);
+        int nadd = add + t[cur].lazy;
+        return query(t[cur].l, b, mid, l, r, nadd) +
+               query(t[cur].r, mid + 1, e, l, r, nadd);
     }
-} st;
+} t;
 
-int a[N];
+int root[N], a[N];
 
 int32_t main()
 {
-    ios::sync_with_stdio(false);
-    cin.tie(nullptr);
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-    int n, q;
-    cin >> n >> q;
+    int n, k;
+    cin >> n >> k;
     for (int i = 1; i <= n; i++)
         cin >> a[i];
 
-    st.build(1, 1, n, a);
-
-    while (q--)
-    {
-        int type;
-        cin >> type;
-        if (type == 1)
-        {
-            int l, r, v;
-            cin >> l >> r >> v;
-            st.upd(1, 1, n, l, r, v);
-        }
-        else
-        {
-            int l, r;
-            cin >> l >> r;
-            cout << st.query(1, 1, n, l, r) << '\n';
-        }
-    }
+    root[0] = t.build(1, n);
 
     return 0;
 }
