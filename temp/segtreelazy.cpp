226934bec4cdf2dcc4dae8e595eab57df568f651
odd.c <tanimahmed710@gmail.com>
2025-12-21 20:48:06 +0600

    a

--- temp/segtreelazy.cpp
+++ temp/segtreelazy.cpp
@@ -1,94 +1,86 @@
 #include <bits/stdc++.h>
 using namespace std;
-
-#define ll long long
-#define all(x) (x).begin(), (x).end()
-#define f(i, n) for (int i = 0; i < n; i++)
-#define trace(x) cerr << #x << ": " << x << '\n'
-const int N = 100007;
-#define int long long
-int arr[N];
-ll t[4 * N], lazy[4 * N];
-ll marge(int l, int r)
+const int N = 5e5 + 9;
+int a[N];
+struct ST
 {
-    return min(t[l], t[r]);
-}
-void push(int n, int b, int e)
-{
-    if (lazy[n] == 0)
-        return;
-    t[n] = t[n] + lazy[n];
-    if (b != e)
+#define lc (n << 1)
+#define rc ((n << 1) + 1)
+    long long t[4 * N], lazy[4 * N];
+    ST()
     {
-        int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-        lazy[l] += lazy[n];
-        lazy[r] += lazy[n];
+        memset(t, 0, sizeof t);
+        memset(lazy, 0, sizeof lazy);
     }
-    lazy[n] = 0;
-}
-void build(int n, int b, int e)
-{
-    lazy[n] = 0;
-    if (b == e)
-    {
-        t[n] = arr[b];
-        return;
+    inline void push(int n, int b, int e)
+    { // change this
+        if (lazy[n] == 0)
+            return;
+        t[n] = t[n] + lazy[n] * (e - b + 1);
+        if (b != e)
+        {
+            lazy[lc] = lazy[lc] + lazy[n];
+            lazy[rc] = lazy[rc] + lazy[n];
+        }
+        lazy[n] = 0;
     }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    build(l, b, mid), build(r, mid + 1, e);
-    t[n] = marge(l, r);
-}
-void upd(int n, int b, int e, int i, int j, int v)
-{
-    push(n, b, e);
-    if (e < i or b > j)
-        return;
-    if (i <= b and j >= e)
+    inline long long combine(long long a, long long b)
+    { // change this
+        return a + b;
+    }
+    inline void pull(int n)
+    { // change this
+        t[n] = t[lc] + t[rc];
+    }
+    void build(int n, int b, int e)
     {
-        lazy[n] = v;
+        lazy[n] = 0; // change this
+        if (b == e)
+        {
+            t[n] = a[b];
+            return;
+        }
+        int mid = (b + e) >> 1;
+        build(lc, b, mid);
+        build(rc, mid + 1, e);
+        pull(n);
+    }
+    void upd(int n, int b, int e, int i, int j, long long v)
+    {
         push(n, b, e);
-        return;
+        if (j < b || e < i)
+            return;
+        if (i <= b && e <= j)
+        {
+            lazy[n] = v; // set lazy
+            push(n, b, e);
+            return;
+        }
+        int mid = (b + e) >> 1;
+        upd(lc, b, mid, i, j, v);
+        upd(rc, mid + 1, e, i, j, v);
+        pull(n);
     }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    upd(l, b, mid, i, j, v), upd(r, mid + 1, e, i, j, v);
-    t[n] = marge(l, r);
-}
-ll query(int n, int b, int e, int i, int j)
-{
-    push(n, b, e);
-    if (e < i or b > j)
-        return INT_MAX;
-    if (i <= b and j >= e)
+    long long query(int n, int b, int e, int i, int j)
     {
-        return t[n];
+        push(n, b, e);
+        if (i > e || b > j)
+            return 0; // return null
+        if (i <= b && e <= j)
+            return t[n];
+        int mid = (b + e) >> 1;
+        return combine(query(lc, b, mid, i, j), query(rc, mid + 1, e, i, j));
     }
-    int mid = (b + e) / 2, l = 2 * n, r = l + 1;
-    return min(query(l, b, mid, i, j), query(r, mid + 1, e, i, j));
-}
+} t;
 int32_t main()
 {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    int n, q;
-    cin >> n >> q;
-    build(1, 1, n);
-    while (q--)
+    int n = 5;
+    for (int i = 1; i <= n; i++)
     {
-        int ty;
-        cin >> ty;
-        if (ty == 1)
-        {
-            int l, r, v;
-            cin >> l >> r >> v;
-            l++;
-            upd(1, 1, n, l, r, v);
-        }
-        else
-        {
-            int i, j;
-            cin >> i >> j;
-            i++;
-            cout << query(1, 1, n, i, j) << '\n';
-        }
+        a[i] = i;
     }
-}
+    t.build(1, 1, n);                       // building the segment tree
+    t.upd(1, 1, n, 2, 3, 10);               // adding 10 to the segment [2, 3]
+    cout << t.query(1, 1, n, 1, 5) << '\n'; // range sum query on the segment [1, 5]
+    return 0;
+}lazy
